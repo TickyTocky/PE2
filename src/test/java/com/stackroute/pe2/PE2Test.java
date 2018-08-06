b58b10a910251771686031ed9698dd4d3fbb493f
TickyTocky <32182417+TickyTocky@users.noreply.github.com>
2018-08-06 10:28:12 +0530

    Final Commit

---
+++ src/test/java/com/stackroute/pe2/PE2Test.java
@@ -0,0 +1,129 @@
+package com.stackroute.pe2;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PE2Test {
+	private static PE2Exercise obj;
+	
+	@BeforeClass
+	public static void setup() {
+		obj = new PE2Exercise();
+	}
+	
+	@AfterClass
+	public static void teardown() {
+		obj = null;
+	}
+	
+	@Test
+	public void isPalindromeTest() {
+
+		assertEquals(true,obj.isPallindrome("abababa"));
+		
+		assertNotEquals(false,obj.isPallindrome("aba"));
+		
+		assertEquals(false,obj.isPallindrome("abc"));
+		
+	}
+
+	@Test
+	public void isPow4Test() {
+
+		assertEquals(true,obj.isPow4(16));
+		
+		assertEquals(false,obj.isPow4(81));
+		
+		assertNotEquals(true,obj.isPow4(10));
+		
+	}
+
+	@Test
+	public void getMemVarTest() {
+
+		assertArrayEquals(new Object[] {"Harry Potter", 30 ,2500.3}, obj.ob.getMemberVariable("Harry Potter", 30 ,2500.3));
+	    
+		assertNotNull(obj.ob.getMemberVariable("Harry Potter", 30 ,2500.3));
+
+		assertNull(null, obj.ob.getMemberVariable(null, 0, 0));
+	}
+	
+	/*@Test
+	public void pallindromeTest() {
+		assertEquals(true, pallindrome.ispallindrome("qwertytrewq"));
+		assertEquals(null, pallindrome.ispallindrome(null));
+		assertEquals(true, pallindrome.ispallindrome("yuipiuy"));
+	}
+	
+	@Test
+	public void pallindromeFailure() {
+		assertNotEquals(false, pallindrome.ispallindrome("asdfghgfdsa"));
+	}
+	
+	@Test
+	public void isPow4Test() {
+		assertEquals(true, pallindrome.isPow4(256));
+	}
+	
+	@Test
+	public void isPow4Failure() {
+		assertNotEquals(false, pallindrome.isPow4(64));
+	}
+	
+	@Test
+	public void MemVarTest() {
+		assertArrayEquals(new Object[] {"Harry Potter", 30, 2500.3}, pallindrome.ob.getMemVar("Harry Potter", 30, 2500.3));
+	}
+	
+	@Test
+	public void MemVarFailure() {
+		assertNotEquals(new Object[] {"Harry Potter", 30, 2500}, pallindrome.ob.getMemVar("Harry Potter", 30, 2500.3));
+		assertNull(null, pallindrome.ob.getMemVar(null, 0, 0));
+	}
+	
+	*/@Test
+	public void isEvenTest() {
+		assertEquals(true, obj.isEven(98));
+		assertEquals(false, obj.isEven(99));
+		assertEquals(false, obj.isEven(101));
+	}
+	
+	@Test
+	public void isEvenFailure() {
+		assertNotEquals(false, obj.isEven(98));
+		assertNotEquals(true, obj.isEven(99));
+		assertNotEquals(true, obj.isEven(101));
+	}
+	
+	@Test
+	public void studentTest() {
+		assertArrayEquals(new double[] {81.50, 65, 98}, obj.stgrad(4,new int[]{86, 65, 98, 77}), 0);
+	}
+	
+	@Test
+	public void studentFailure() {
+		assertNotEquals(new double[] {81, 65, 98}, obj.stgrad(4,new int[]{86, 65, 98, 77}));
+	}
+	
+	@Test
+	public void factTest() {
+		assertEquals(3628800, obj.longFactorial(10));
+		assertEquals(1, obj.longFactorial(1));
+		assertEquals(120, obj.longFactorial(5));
+	}
+	
+	@Test
+	public void factFailure() {
+		assertNotEquals(62270208, obj.longFactorial(1));
+		assertNotEquals(62270208, obj.longFactorial(13));
+		assertNotEquals(62270208, obj.longFactorial(13));
+	}
+	
+}
